2b2054028e9807663347152cfb9a2a4171e0b84f
brent <pnxrock101@gmail.com>
2021-04-16 08:53:19 -0700

    Added code to retreave jokes from chuck norris api

--- joke.py
+++ joke.py
@@ -1,1 +1,13 @@
 ## This will be an api call to a url for jokes
+
+import requests
+
+url = "https://api.icndb.com/jokes/random"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text + "\n")
+print("^Now that's funny.")
